dc1a056a5778b1a529ac52689621290aa971697d
Alex Arguelles <alexarguelles87923@gmail.com>
2023-06-22 22:14:44 -0400

    fixing sign up

---
+++ front-end/src/components/Backup/SignUpBody/SignUpBody.js
@@ -0,0 +1,161 @@
+import React, { useState } from 'react';
+import { Wrapper } from './SignUpBodyStyles';
+
+function SignUp() {
+  const divStyle = {
+    zIndex: 10
+  };
+
+  const h1Style = {
+    color: 'hsl(218, 81%, 95%)'
+  };
+
+  const spanStyle = {
+    color: 'hsl(218, 81%, 75%)'
+  };
+
+  const pStyle = {
+    color: 'hsl(218, 81%, 85%)'
+  };
+
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: ''
+  });
+
+  const handleChange = e => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+
+    const response = await fetch('/api/v1/sign-up', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(formData)
+    });
+
+    if (response.ok) {
+      console.log('Response successful!');
+    } else {
+      console.error('An error occurred.');
+    }
+  };
+
+  return (
+    <Wrapper>
+      <section className="background-radial-gradient overflow-hidden">
+        <div className="container px-4 py-5 px-md-5 text-center text-lg-start my-5">
+          <div className="row gx-lg-5 align-items-center mb-5">
+            <div className="col-lg-6 mb-5 mb-lg-0" style={divStyle}>
+              <h1 className="my-5 display-5 fw-bold ls-tight" style={h1Style}>
+                Welcome to <br />
+                <span style={spanStyle}>Termitomator</span>
+              </h1>
+              <p className="mb-4 opacity-70" style={pStyle}>
+                MDC North AI & Robotics Club invites you to a fast-paced and
+                exciting introduction into robotics. In this event, you'll get
+                the chance to create your own robot that will compete to protect
+                a precious tomato while trying to attack your opponent's. The
+                event is open to middle and high school students and is an
+                excellent opportunity to showcase your creativity and
+                engineering skills. All funds raised will go towards supporting
+                our club and the young robot designers. We can't wait to see
+                what you'll create!
+              </p>
+            </div>
+            <div className="col-lg-6 mb-5 mb-lg-0 position-relative">
+              <div
+                id="radius-shape-1"
+                className="position-absolute rounded-circle shadow-5-strong"
+              ></div>
+              <div
+                id="radius-shape-2"
+                className="position-absolute shadow-5-strong"
+              ></div>
+              <div className="card bg-glass">
+                <div className="card-body px-4 py-5 px-md-5">
+                  <form onSubmit={handleSubmit}>
+                    <div className="row">
+                      <div className="col-md-6 mb-4">
+                        <div className="form-outline">
+                          <input
+                            type="text"
+                            id="form3Example1"
+                            className="form-control"
+                            name="firstName"
+                            value={formData.firstName}
+                            onChange={handleChange}
+                          />
+                          <label className="form-label" htmlFor="form3Example1">
+                            First name
+                          </label>
+                        </div>
+                      </div>
+                      <div className="col-md-6 mb-4">
+                        <div className="form-outline">
+                          <input
+                            type="text"
+                            id="form3Example2"
+                            className="form-control"
+                            name="lastName"
+                            value={formData.lastName}
+                            onChange={handleChange}
+                          />
+                          <label className="form-label" htmlFor="form3Example2">
+                            Last name
+                          </label>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="form-outline mb-4">
+                      <input
+                        type="email"
+                        id="form3Example3"
+                        className="form-control"
+                        name="email"
+                        value={formData.email}
+                        onChange={handleChange}
+                      />
+                      <label className="form-label" htmlFor="form3Example3">
+                        Email address
+                      </label>
+                    </div>
+                    <div className="form-outline mb-4">
+                      <input
+                        type="password"
+                        id="form3Example4"
+                        className="form-control"
+                        name="password"
+                        value={formData.password}
+                        onChange={handleChange}
+                      />
+                      <label className="form-label" htmlFor="form3Example4">
+                        Password
+                      </label>
+                    </div>
+                    <div className="form-check d-flex justify-content-center mb-4">
+                      <button
+                        type="submit"
+                        className="btn btn-primary btn-block mb-4"
+                      >
+                        Sign up
+                      </button>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </Wrapper>
+  );
+}
+
+export default SignUp;
--- front-end/src/components/SignUpBody/SignUpBody.js
+++ front-end/src/components/SignUpBody/SignUpBody.js
@@ -1,160 +1,117 @@
-import React, { useState } from 'react';
-import { Wrapper } from './SignUpBodyStyles';
+import React from 'react';
+import { BootstrapWrapper } from '../BootstrapWrapper/BootstrapWrapper';
 
 function SignUp() {
-  const divStyle = {
-    zIndex: 10
-  };
-
-  const h1Style = {
-    color: 'hsl(218, 81%, 95%)'
-  };
-
-  const spanStyle = {
-    color: 'hsl(218, 81%, 75%)'
-  };
-
-  const pStyle = {
-    color: 'hsl(218, 81%, 85%)'
-  };
-
-  const [formData, setFormData] = useState({
-    firstName: '',
-    lastName: '',
-    email: '',
-    password: ''
-  });
-
-  const handleChange = e => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
-
-  const handleSubmit = async e => {
-    e.preventDefault();
-
-    const response = await fetch('/api/v1/sign-up', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(formData)
-    });
-
-    if (response.ok) {
-      console.log('Response successful!');
-    } else {
-      console.error('An error occurred.');
-    }
-  };
-
   return (
-    <Wrapper>
-      <section className="background-radial-gradient overflow-hidden">
-        <div className="container px-4 py-5 px-md-5 text-center text-lg-start my-5">
-          <div className="row gx-lg-5 align-items-center mb-5">
-            <div className="col-lg-6 mb-5 mb-lg-0" style={divStyle}>
-              <h1 className="my-5 display-5 fw-bold ls-tight" style={h1Style}>
-                Welcome to <br />
-                <span style={spanStyle}>Termitomator</span>
-              </h1>
-              <p className="mb-4 opacity-70" style={pStyle}>
-                MDC North AI & Robotics Club invites you to a fast-paced and
-                exciting introduction into robotics. In this event, you'll get
-                the chance to create your own robot that will compete to protect
-                a precious tomato while trying to attack your opponent's. The
-                event is open to middle and high school students and is an
-                excellent opportunity to showcase your creativity and
-                engineering skills. All funds raised will go towards supporting
-                our club and the young robot designers. We can't wait to see
-                what you'll create!
-              </p>
-            </div>
-            <div className="col-lg-6 mb-5 mb-lg-0 position-relative">
-              <div
-                id="radius-shape-1"
-                className="position-absolute rounded-circle shadow-5-strong"
-              ></div>
-              <div
-                id="radius-shape-2"
-                className="position-absolute shadow-5-strong"
-              ></div>
-              <div className="card bg-glass">
-                <div className="card-body px-4 py-5 px-md-5">
-                  <form onSubmit={handleSubmit}>
-                    <div className="row">
-                      <div className="col-md-6 mb-4">
-                        <div className="form-outline">
-                          <input
-                            type="text"
-                            id="form3Example1"
-                            className="form-control"
-                            name="firstName"
-                            value={formData.firstName}
-                            onChange={handleChange}
-                          />
-                          <label className="form-label" htmlFor="form3Example1">
-                            First name
-                          </label>
-                        </div>
+    <BootstrapWrapper>
+      <section
+        className="vh-100 bg-image"
+        // style={{
+        //   backgroundImage: url(
+        //     'https://mdbcdn.b-cdn.net/img/Photos/new-templates/search-box/img4.webp'
+        //   )
+        // }}
+      >
+        <div className="mask d-flex align-items-center h-100 gradient-custom-3">
+          <div className="container h-100">
+            <div className="row d-flex justify-content-center align-items-center h-100">
+              <div className="col-12 col-md-9 col-lg-7 col-xl-6">
+                <div className="card" style={{ borderRadius: '15px' }}>
+                  <div className="card-body p-5">
+                    <h2 className="text-uppercase text-center mb-5">
+                      Create an account
+                    </h2>
+
+                    <form>
+                      <div className="form-outline mb-4">
+                        <input
+                          type="text"
+                          id="form3Example1cg"
+                          className="form-control form-control-lg"
+                        />
+                        <label className="form-label" htmlFor="form3Example1cg">
+                          Your Name
+                        </label>
                       </div>
-                      <div className="col-md-6 mb-4">
-                        <div className="form-outline">
-                          <input
-                            type="text"
-                            id="form3Example2"
-                            className="form-control"
-                            name="lastName"
-                            value={formData.lastName}
-                            onChange={handleChange}
-                          />
-                          <label className="form-label" htmlFor="form3Example2">
-                            Last name
-                          </label>
-                        </div>
+
+                      <div className="form-outline mb-4">
+                        <input
+                          type="email"
+                          id="form3Example3cg"
+                          className="form-control form-control-lg"
+                        />
+                        <label className="form-label" htmlFor="form3Example3cg">
+                          Your Email
+                        </label>
                       </div>
-                    </div>
-                    <div className="form-outline mb-4">
-                      <input
-                        type="email"
-                        id="form3Example3"
-                        className="form-control"
-                        name="email"
-                        value={formData.email}
-                        onChange={handleChange}
-                      />
-                      <label className="form-label" htmlFor="form3Example3">
-                        Email address
-                      </label>
-                    </div>
-                    <div className="form-outline mb-4">
-                      <input
-                        type="password"
-                        id="form3Example4"
-                        className="form-control"
-                        name="password"
-                        value={formData.password}
-                        onChange={handleChange}
-                      />
-                      <label className="form-label" htmlFor="form3Example4">
-                        Password
-                      </label>
-                    </div>
-                    <div className="form-check d-flex justify-content-center mb-4">
-                      <button
-                        type="submit"
-                        className="btn btn-primary btn-block mb-4"
-                      >
-                        Sign up
-                      </button>
-                    </div>
-                  </form>
+
+                      <div className="form-outline mb-4">
+                        <input
+                          type="password"
+                          id="form3Example4cg"
+                          className="form-control form-control-lg"
+                        />
+                        <label className="form-label" htmlFor="form3Example4cg">
+                          Password
+                        </label>
+                      </div>
+
+                      <div className="form-outline mb-4">
+                        <input
+                          type="password"
+                          id="form3Example4cdg"
+                          className="form-control form-control-lg"
+                        />
+                        <label
+                          className="form-label"
+                          htmlFor="form3Example4cdg"
+                        >
+                          Repeat your password
+                        </label>
+                      </div>
+
+                      <div className="form-check d-flex justify-content-center mb-5">
+                        <input
+                          className="form-check-input me-2"
+                          type="checkbox"
+                          value=""
+                          id="form2Example3cg"
+                        />
+                        <label
+                          className="form-check-label"
+                          htmlFor="form2Example3g"
+                        >
+                          I agree all statements in{' '}
+                          <a href="#!" className="text-body">
+                            <u>Terms of service</u>
+                          </a>
+                        </label>
+                      </div>
+
+                      <div className="d-flex justify-content-center">
+                        <button
+                          type="button"
+                          className="btn btn-success btn-block btn-lg gradient-custom-4 text-body"
+                        >
+                          Register
+                        </button>
+                      </div>
+
+                      <p className="text-center text-muted mt-5 mb-0">
+                        Have already an account?{' '}
+                        <a href="#!" className="fw-bold text-body">
+                          <u>Login here</u>
+                        </a>
+                      </p>
+                    </form>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
       </section>
-    </Wrapper>
+    </BootstrapWrapper>
   );
 }
 
