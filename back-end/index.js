80735a92f4fb0d9f8c05faab64e5901b43b89d8f
Alex Arguelles <alexarguelles87923@gmail.com>
2023-08-09 14:53:44 -0400

    prod

--- back-end/index.js
+++ back-end/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-const port = process.env.PORT
+const port = process.env.PORT || 3001
 const connect_to_db = require('./database/database');
 const path = require("path")
 const bodyParser = require('body-parser');
